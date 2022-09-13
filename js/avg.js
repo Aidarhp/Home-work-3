e428c19fd5ddf3fc5f8b31472547559124680d59
Aidarhp <112775850+Aidarhp@users.noreply.github.com>
2022-09-13 20:33:44 +0600

    Add files via upload

---
+++ js/avg.js
@@ -0,0 +1,19 @@
+function ruby(a, b, c, d,) {
+    return a + b + c + d ;
+}
+let num = ruby(1,2,3,4,);
+console.log(num / 4);
+
+
+function crab(a,b,c,d,i) {
+    return a + b + c + d + i;
+}
+let result = crab(1,2,3,4,5);
+console.log(result / 5);
+
+
+function lock() {
+    return 0;
+}
+let red = lock();
+console.log(red / 4);
